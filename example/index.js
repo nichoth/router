fd0aa9bfaa6b5686259aa0e2126a42bac9af06d7
Nick Thomas <nichoth@gmail.com>
2018-02-01 16:50:54 -0800

    trying more things

--- example/index.js
+++ example/index.js
@@ -1,30 +1,41 @@
 var App = require('../')
-
 var app = App({ hello: 'world' })
+app.on('test', console.log.bind('in here'))
 
 app.onRouteMatch(function (view) {
-    console.log('here', view)
+    // this gets called with the return value from the route function
+    console.log('on match --', view)
 })
 
 app.route('/', function root (params) {
-    console.log('hello', app.hello)
-    console.log('params', params)
-    return 'woo root view'
+    console.log('in root --', app.hello, params)
+    return 'root view'
 })
 
+app.route('/bar', BarRoute(app))
+
+app.route('/foo', FooRoute())
+
+function BarRoute (app) {
+    return function (params) {
+        app.emit('test', 'aaaa')
+        return 'bar view'
+    }
+}
+
 function FooRoute () {
+    if (!(this instanceof FooRoute)) return new FooRoute()
     this.foo = 'woo'
-    this.state = 'blar'
-    this.view = 'ham crown'
-    console.log('this.name', this.constructor.name)
+    this.state = 'foo state'
+    this.view = 'foo view'
+    // console.log('this.name', this.constructor.name)
 }
 
 FooRoute.prototype.onMatch = function (params) {
     console.log('foo route', this.foo)
-    return 'barrrr foo view'
+    return 'foo view'
 }
 
-app.route('/foo', new FooRoute())
 
 
 
--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 var Routes = require('routes')
-var onRoute = require('route-event')
-var Bus = require('nanobus')
+var OnRoute = require('route-event')
+var Bus = require('@nichoth/events')
 var inherits = require('inherits')
 var mxtend = require('xtend/mutable')
 
 function Router (opts) {
     if (!(this instanceof Router)) return new Router(opts)
+    var onRoute = OnRoute()
     mxtend(this, opts || {})
     var self = this
     this._routes = Routes()
     this._fns = []
 
-    this.setRoute = onRoute(function (path) {
+    this.setRoute = onRoute(function onChange (path) {
+        console.log('in here', path)
         var r = self._routes.match(path)
         if (!r) throw new Error('Unhandled route, ' + path)
         var val = r.fn(r.params)
@@ -27,8 +29,9 @@ Router.prototype.onRouteMatch = function (fn) {
 }
 
 Router.prototype.route = function (path, fn) {
-    this._routes.addRoute(path, typeof fn === 'function' ?  fn :
-        fn.onMatch.bind(fn))
+    this._routes.addRoute(path, typeof fn === 'function' ?
+        fn :
+        function () { fn.onMatch.apply(fn, arguments) })
 }
 
 module.exports = Router
